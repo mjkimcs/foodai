e94ae388166a56944ea3fb6a7e096de56a94882a
Kim Min Jeong <77471280+mjkimcs@users.noreply.github.com>
2021-12-11 16:30:22 +0900

    Add files via upload

--- streamlit_foodai_final.py
+++ streamlit_foodai_final.py
@@ -2,9 +2,9 @@ import streamlit as st
 from PIL import Image
 import pandas as pd
 import numpy as np
-import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras.models import load_model
+# import tensorflow as tf
+# from tensorflow import keras
+# from tensorflow.keras.models import load_model
 
 # https://docs.streamlit.io/
 # streamlit run streamlit_foodai_final.py
